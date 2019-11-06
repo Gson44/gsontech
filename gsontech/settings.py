90135b03911ec8f6c297a4bca58e9096d2a6a5f8
Gerson <Geric99997@gmail.com>
2019-11-05 22:49:59 -0500

    add contact page

--- gsontech/settings.py
+++ gsontech/settings.py
@@ -118,3 +118,10 @@ STATICFILES_DIRS = [
 ]
 django_heroku.settings(locals())
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'garec001@fiu.edu'
+EMAIL_HOST_PASSWORD = 'KnoWisPow1000!!!'
+
--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('',views.index, name="home"),
     path('projects/',views.projects_view, name="projects"),
     path('web/',views.web_view, name="web"),
-     path('iot/',views.iot_view, name="iot"),
+    path('iot/',views.iot_view, name="iot"),
+    path('sent/',views.send_view, name="sent"),
+    path('success/', views.successView, name='success'),
+
 ]
